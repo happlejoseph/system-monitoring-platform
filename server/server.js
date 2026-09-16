8d4982b5117d98b1c5dd55f0991fd1998c6d03d9
Shreya036 <shreyashetty031201>
2026-09-16 10:09:52 +0530

    ongoose

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 
 
 import express from "express";
+import connectDB from './src/congig/db.js'
 
+connectDB();
 
 const app = express();
 
--- server/src/congig/db.js
+++ server/src/congig/db.js
@@ -3,3 +3,15 @@
 import mongoose from "mongoose";
 
 
+const connectDB = async()=> {
+
+    try {
+
+        await mongoose.connect(process.env.MONGO_URL);
+    }
+    catch(error) {
+        console.error('mongodb connection failed:', error.message);
+    }
+}
+
+export default connectDB;
