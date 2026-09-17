b9c14c084c1b5e9052ba18f45355c5f545397c7d
happlejoseph <happlejoseph66@gmail.com>
2026-09-17 12:44:36 +0530

    add user

---
+++ server/src/controllers/userController.js
@@ -0,0 +1,48 @@
+
+
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const addUser = async(req, res)=> {
+
+    try {
+
+        const {name, email, password, role} = req.body;
+
+        if(!name || !email || !password || !role) {
+            return res.status(400).json({
+                message: 'All fields are required'
+            });
+        }
+
+        const existingUser = await User.findOne({email});
+
+        if(existingUser) {
+            return res.status(400).json({
+                message: 'User is already exists'
+            });
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name, email, password: hashPassword, role
+        });
+
+        res.status(201).json({
+            message: 'User created successfully',
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        });
+    }
+
+    catch(error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
