eb8f9e36f3dc30a12a0148ea123d0b5908e7009f
happlejoseph <happlejoseph66@gmail.com>
2026-09-20 21:14:03 +0530

    get server and get server by id

--- server/src/controllers/serverController.js
+++ server/src/controllers/serverController.js
@@ -39,3 +39,52 @@ export const addServer = async(req, res)=> {
         });
     }
 }
+
+
+export const getServers = async(req, res)=> {
+
+    try {
+        
+        const servers = await Server.find();
+
+        res.status(201).json({
+            message: 'Servers fetches successfully',
+            servers
+        });
+    }
+
+    catch(error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+
+
+export const getServerById = async(req, res)=> {
+
+    try {
+
+        const {id} = req.params;
+
+        const server = await Server.findById(id);
+
+        if(!server) {
+            return res.status(401).json({
+                message: 'Server not found'
+            });
+        }
+
+        res.status(201).json({
+            message: 'Server fetched successfully',
+            server
+        });
+    }
+
+    catch(error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
--- server/src/routes/serverRoutes.js
+++ server/src/routes/serverRoutes.js
@@ -3,11 +3,15 @@
 import express from 'express';
 import { auth } from '../middleware/authMiddleware';
 import { allowRoles } from '../middleware/roleMiddleware';
-import { addServer } from '../controllers/serverController';
+import { addServer, getServerById, getServers } from '../controllers/serverController';
 
 
 const router = express.Router();
 
 router.post('/', auth, allowRoles('admin'), addServer);
+
+router.get('/', auth, allowRoles('admin'), getServers);
+
+router.get('/:id', auth, allowRoles('admin'), getServerById);
 
 export default router;
