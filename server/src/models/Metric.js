a23948b15345b0e8e12b4416b0e6f8f56c9b0fed
happlejoseph <happlejoseph66@gmail.com>
2026-09-21 13:24:02 +0530

    metric schema

---
+++ server/src/models/Metric.js
@@ -0,0 +1,60 @@
+
+
+import mongoose from "mongoose";
+
+
+const MetricSchema = new mongoose.Schema({
+    
+    server: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Server',
+        required: true
+    },
+
+    cpu: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 100
+    },
+
+    memory: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 100
+    },
+
+    memory: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 100
+    },
+
+    disk: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 100
+    },
+
+    temperature: {
+        type: Number,
+        required: true
+    },
+
+    fanSpeed: {
+        type: Number,
+        required: true
+    },
+
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+}, {timestamps: true})
+
+const Metric = mongoose.model('Metric', MetricSchema);
+
+export default Metric;
